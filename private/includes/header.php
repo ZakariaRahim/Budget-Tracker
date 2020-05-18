722392ef9bfc10dfd9ab21e89116a335d89d028c
MahmudSuleman <mudisheikh@outlook.com>
2020-05-18 23:40:37 +0000

    file deleted

--- private/includes/header.php
+++ private/includes/header.php
@@ -5,13 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <script src="../js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="file:///C:/xampp/htdocs/dashboard/asana/includes/js/bootstrap.min.js"></script>
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="../../public/css/bootstrap.min.css" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="../asana/fontawesome-free-5.10.1-web/css/all.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="fontawesome-free-5.10.1-web/css/all.css" type="text/css">
+    <link href="../../public/css/fontawesome-free-5.10.1-web/css/all.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../../public/style.css">
     <style>
     </style>
 </head>
