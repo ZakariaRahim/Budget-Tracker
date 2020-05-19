3194eb9a6cbdb37f9ba316514ef6d2a4ab51bc00
MahmudSuleman <mudisheikh@outlook.com>
2020-05-19 05:36:49 +0000

    style for index. php changed...

--- public/index.php
+++ public/index.php
@@ -1,42 +1,31 @@
     <?php
     include_once '../private/init.php';
+    $title = "Login";
     include_once SHARED_PATH .'/header.php';
     
     ?>
-    <link rel="stylesheet" href="../styles/login.css">
     <div class="container-fluid">
         <div class="login-form">
-            <h4>Please Enter Your Details to Login</h4>
+            
             <form action="Login.html" method="post">
-                <div class="rap-content">
-                    <div class="form-group">
-                     
-                        <input type="text" class="form-control" placeholder="User Name or Id">   <i class="fas fa-sign-in"></i>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="department" placeholder="Password"
-                            required="required">
-                    </div>
+                <p class='login-title'>login here</p>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="UserId">   <i class="fas fa-sign-in"></i>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="department" placeholder="Password"
+                        required="required">
+                </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-success btn-lg btn-block" style="background-color:black"
-                            name="bsubmit">Login </button>
-
-                    </div>
-
-                    <div class="footer">
-                    <?php
-    include_once SHARED_PATH .'/footer.php';
-    
-    ?>
-                    </div>
-
+                <div class="form-group">
+                    <button type="submit" class="btn btn-login btn-block" 
+                        name="submit">Login </button>
+                </div>
             </form>
-
-
         </div>
 
     </div>
   
-
+<?php    //include_once SHARED_PATH .'/footer.php';  ?>
+           
    
