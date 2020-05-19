ebf9f6285b0b636090b1b273e47bff534a1c0018
MahmudSuleman <mudisheikh@outlook.com>
2020-05-19 07:20:25 +0000

    login functionality implemented

--- public/index.php
+++ public/index.php
@@ -2,18 +2,27 @@
     include_once '../private/init.php';
     $title = "Login";
     include_once SHARED_PATH .'/header.php';
+
+
+    if(isset($_POST['submit'])){
+
+        $id = $_POST['user_id'] ?? '';
+        $password = $_POST['password'] ?? '';
+        User::signin($id, $password);
+    }
     
     ?>
     <div class="container-fluid">
         <div class="login-form">
-            
-            <form action="login.php" method="post">
+            <?php var_dump($error); ?>
+            <form action="index.php" method="post">
                 <p class='login-title'>login here</p>
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="UserId" required>   <i class="fas fa-sign-in"></i>
+                    <input type="text" class="form-control"
+                    name="user_id" placeholder="UserId" required>
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" name="department" placeholder="Password"
+                    <input type="password" class="form-control" name="password" placeholder="Password"
                         required="required">
                 </div>
 
