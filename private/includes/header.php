1dc3b9c4a20c48fbcd63fdf9dd535237d6b7bc97
MahmudSuleman <mudisheikh@outlook.com>
2020-05-19 05:06:14 +0000

    css files added to the header.php file

--- private/includes/header.php
+++ private/includes/header.php
@@ -7,9 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $title; ?></title>
     <link rel="stylesheet" href="../../public/css/bootstrap.min.css" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    
     <link href="../../public/css/fontawesome-free-5.10.1-web/css/all.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="../../public/style.css">
+    <link rel="stylesheet" href="../../public/style/budget.css">
+    <link rel="stylesheet" href="../../public/style/budgetapproval.css">
+    <link rel="stylesheet" href="../../public/style/login.css">
+    <link rel="stylesheet" href="../../public/style/signup.css">
+    <link rel="stylesheet" href="../../public/style/userdash.css">
     <style>
     </style>
 </head>
