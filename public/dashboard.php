8475078a4b31bb4482fbff85cc5e539e8bfceef0
MahmudSuleman <mudisheikh@outlook.com>
2020-05-19 07:19:58 +0000

    dashboard file created.. user and admin dashboard to be combined

---
+++ public/dashboard.php
@@ -0,0 +1,10 @@
+<?php 
+include_once '../../private/init.php';
+$title = 'Dashboard.php';
+include_once SHARED_PATH . '/header.php';
+?>
+
+
+<div class="container">
+    <!-- content goes in here -->
+</div>
